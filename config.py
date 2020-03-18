5b6241f170111a5ab49e09bb7c0edd136ff0240f
nyx <nyx@nightstonecastle.local>
2020-03-18 07:52:35 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+#Config file for threading. remove this.
+service = {
+    'url' : 'http://localhost/wnyc'
+}
---
+++ snd.py
@@ -0,0 +1,28 @@
+#Simple Radio Server
+#A multithreaded application to record and serve a shoutcast radio stream from an SDR
+# =( Coronavirus
+import threading
+import time
+import requests #Maybe i wont use this
+import config
+import logging
+
+
+#class recordHour(threading.Thread):
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
